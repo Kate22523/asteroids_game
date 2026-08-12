bfc0b6ea21ce70971322f2e67710b8d28b72e4cd
Kate22523 <levson17518@gmail.com>
2026-08-12 18:35:05 +0300

    draws the player

--- asteroids_game/main.py
+++ asteroids_game/main.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
@@ -7,13 +7,15 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt: float = 0.0
+    Player = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return   
-        def fill(black):
-            pygame.display.flip()
+        screen.fill("black")
+        Player.draw(screen)
+        pygame.display.flip()
         dt = clock.tick(60) / 1000
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
---
+++ asteroids_game/player.py
@@ -0,0 +1,15 @@
+import pygame, circleshape
+from constants import PLAYER_RADIUS
+
+class Player(circleshape.CircleShape):
+    def __init__(self, x: float, y: float):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+    # in the Player class
+    def triangle(self) -> list[pygame.Vector2]:
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
