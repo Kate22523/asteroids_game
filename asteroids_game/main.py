98ed01f483e6ea7e8b28be9f451fe1632d228b22
Kate22523 <levson17518@gmail.com>
2026-08-16 13:03:49 +0300

    added moving up and down and changed the architechure to groups

--- asteroids_game/main.py
+++ asteroids_game/main.py
@@ -1,4 +1,5 @@
-import pygame, player
+import pygame
+from player import Player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
@@ -7,15 +8,19 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt: float = 0.0
-    Player = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return   
         screen.fill("black")
-        Player.update(dt)
-        Player.draw(screen)
+        updatable.update(dt)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
